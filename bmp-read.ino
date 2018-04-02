dc7f75096fe01b9d38f2258a41497073792ca3ae
Martin <martinius96@gmail.com>
2018-04-03 00:11:07 +0200

    stack overflow fix + minify

--- bmp-read.ino
+++ bmp-read.ino
@@ -1,24 +1,18 @@
 #include "Adafruit_BMP280.h"         //bmp280 kniznica s upravou na 0x76 adresu
 Adafruit_BMP280 bmp; //inicializacia BMP senzora
 void setup() {          
-  bmp.begin();        //start snimaca BMP
-                   
+  bmp.begin();        //start snimaca BMP                  
   Serial.begin(9600);    //SPUSTENIE SERIOVEJ LINKY --UART-- NA CITACIU RYCHLOST 9600
-  while (!Serial) {
-    ;                                        
-  }
-
-
-
 }
-
-void loop() {                                      //ZACIATOK SLUCKY
+void loop() {
   
-    Serial.println("Atmosfericky tlak: ");           //SERIOVY VYPIS TEXT O TLAKU VZDUCHU
-    Serial.println((bmp.readPressure() / 100) + 103,855); //SERIOVY VYPIS STAVU RELATIVNEHO TLAKU 30,... je konstanta pre nadmorsku vysku, ktora sa prirata k teplote. (Použite pri nadmorskej do 1000m nadmorska vyska/8,3 tuto hodnotu napiste namiesto 30,...)
+    Serial.println("Atmosfericky tlak: "); 
+    Serial.println((bmp.readPressure() / 100) + 103.855);
+//103.855 je konštanta pre 862mnm. Pre správnu konštantu urobte prevod vaša nadmorská výška/8,3. Túto konštantu pripočítajte k meraniu.
+//spoľahlivé do výšky 1000mnm.
     Serial.println("Teplota: "); 
     Serial.println(bmp.readTemperature());
     
 
-  delay(5000); //15 SEKUND PAUZA, slucka sa zopakuje
+  delay(5000); //5 SEKUND PAUZA
 }
